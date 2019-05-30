31b6325a4300271c060cb1100dd46a4e4dccb6ed
Cesar Lorenzo <clorenzo@grtle.com>
2019-05-30 15:18:24 -0500

    Events

--- FingerPrintValidation/FingerPrintValidation/FingerValidationView.Designer.cs
+++ FingerPrintValidation/FingerPrintValidation/FingerValidationView.Designer.cs
@@ -28,21 +28,260 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.img_FingerPrint = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_Ruc = new System.Windows.Forms.Label();
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.lbl_Prompt = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.img_FingerPrint)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lbl_Prompt, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(384, 361);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // img_FingerPrint
+            // 
+            this.img_FingerPrint.BackColor = System.Drawing.Color.White;
+            this.img_FingerPrint.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.img_FingerPrint.Location = new System.Drawing.Point(10, 10);
+            this.img_FingerPrint.Margin = new System.Windows.Forms.Padding(10);
+            this.img_FingerPrint.Name = "img_FingerPrint";
+            this.img_FingerPrint.Size = new System.Drawing.Size(169, 215);
+            this.img_FingerPrint.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img_FingerPrint.TabIndex = 0;
+            this.img_FingerPrint.TabStop = false;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.img_FingerPrint, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel4, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 83);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(378, 235);
+            this.tableLayoutPanel2.TabIndex = 5;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.19894F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 74.80106F));
+            this.tableLayoutPanel3.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.lbl_Ruc, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.lbl_Name, 1, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(12, 12, 12, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(360, 65);
+            this.tableLayoutPanel3.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DimGray;
+            this.label1.Location = new System.Drawing.Point(1, 1);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.label1.Size = new System.Drawing.Size(89, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RUC";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DimGray;
+            this.label2.Location = new System.Drawing.Point(1, 33);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.label2.Size = new System.Drawing.Size(89, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cliente";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbl_Ruc
+            // 
+            this.lbl_Ruc.AutoSize = true;
+            this.lbl_Ruc.BackColor = System.Drawing.Color.White;
+            this.lbl_Ruc.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl_Ruc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Ruc.Location = new System.Drawing.Point(91, 1);
+            this.lbl_Ruc.Margin = new System.Windows.Forms.Padding(0);
+            this.lbl_Ruc.Name = "lbl_Ruc";
+            this.lbl_Ruc.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.lbl_Ruc.Size = new System.Drawing.Size(268, 31);
+            this.lbl_Ruc.TabIndex = 2;
+            this.lbl_Ruc.Text = "3373234";
+            this.lbl_Ruc.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.BackColor = System.Drawing.Color.White;
+            this.lbl_Name.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Name.Location = new System.Drawing.Point(91, 33);
+            this.lbl_Name.Margin = new System.Windows.Forms.Padding(0);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.lbl_Name.Size = new System.Drawing.Size(268, 31);
+            this.lbl_Name.TabIndex = 3;
+            this.lbl_Name.Text = "Carlos Alberto Marquinez Nunez";
+            this.lbl_Name.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 2;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.Controls.Add(this.btn_Clear, 0, 1);
+            this.tableLayoutPanel4.Controls.Add(this.btn_Cancel, 1, 1);
+            this.tableLayoutPanel4.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(199, 10);
+            this.tableLayoutPanel4.Margin = new System.Windows.Forms.Padding(10);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 2;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(169, 215);
+            this.tableLayoutPanel4.TabIndex = 0;
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_Clear.Location = new System.Drawing.Point(0, 175);
+            this.btn_Clear.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(84, 40);
+            this.btn_Clear.TabIndex = 0;
+            this.btn_Clear.Text = "Borrar";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.Btn_Clear_Click);
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_Cancel.Location = new System.Drawing.Point(84, 175);
+            this.btn_Cancel.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(85, 40);
+            this.btn_Cancel.TabIndex = 1;
+            this.btn_Cancel.Text = "Cancelar";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            this.btn_Cancel.Click += new System.EventHandler(this.Btn_Cancel_Click);
+            // 
+            // lbl_Prompt
+            // 
+            this.lbl_Prompt.AutoSize = true;
+            this.lbl_Prompt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl_Prompt.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Prompt.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lbl_Prompt.Location = new System.Drawing.Point(3, 321);
+            this.lbl_Prompt.Name = "lbl_Prompt";
+            this.lbl_Prompt.Size = new System.Drawing.Size(378, 40);
+            this.lbl_Prompt.TabIndex = 7;
+            this.lbl_Prompt.Text = "No se puede iniciar la captura!";
+            this.lbl_Prompt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DarkGray;
+            this.tableLayoutPanel4.SetColumnSpan(this.pictureBox1, 2);
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(20, 20);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(129, 135);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // FingerValidationView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FingerValidationView";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.img_FingerPrint)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.tableLayoutPanel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox img_FingerPrint;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_Ruc;
+        private System.Windows.Forms.Label lbl_Name;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.Button btn_Clear;
+        private System.Windows.Forms.Button btn_Cancel;
+        private System.Windows.Forms.Label lbl_Prompt;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- FingerPrintValidation/FingerPrintValidation/FingerValidationView.cs
+++ FingerPrintValidation/FingerPrintValidation/FingerValidationView.cs
@@ -7,14 +7,103 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DPFP;
+using DPFP.Capture;
 
 namespace FingerPrintValidation
 {
-    public partial class FingerValidationView : Form
+    public partial class FingerValidationView : Form, DPFP.Capture.EventHandler
     {
+        private Capture Capturer;
         public FingerValidationView()
         {
             InitializeComponent();
+
+            ActiveControl = label1;
+
+            Init(Capturer);
         }
+
+        private void Btn_Clear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Cancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void OnComplete(object Capture, string ReaderSerialNumber, Sample Sample)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnFingerGone(object Capture, string ReaderSerialNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnFingerTouch(object Capture, string ReaderSerialNumber)
+        {
+            MessageBox.Show("El Lector de huellas fue tocado.");
+        }
+
+        public void OnReaderConnect(object Capture, string ReaderSerialNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnReaderDisconnect(object Capture, string ReaderSerialNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnSampleQuality(object Capture, string ReaderSerialNumber, CaptureFeedback CaptureFeedback)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region GRE_Function
+
+        protected virtual void Init(Capture Capturer)
+        {
+            try
+            {
+                Capturer = new Capture();
+
+                if (null != Capturer)
+                {
+                    Capturer.EventHandler = this;
+
+                    SetPrompt("Iniciando operación de captura...");
+                }
+                else
+                {
+                    SetPrompt("No se puede iniciar la operación de captura.");
+                }
+            }
+            catch
+            {
+                SetPrompt("Error al iniciar la operación de captura.");
+            }
+        }
+
+        private void SetPrompt(string message)
+        {
+            lbl_Prompt.Text = "";
+            lbl_Prompt.Text = message;
+        }
+
+        private void DrawingCapture(Bitmap bitmap)
+        {
+            Invoke(new Function(delegate () {
+                img_FingerPrint.Image = new Bitmap(bitmap, img_FingerPrint.Size);
+            }));
+        }
+
+        delegate void Function();
+
+        #endregion
     }
 }
